989ad9daef4cc78a927c51a4f5c73bb53c45fef0
Amritgrg1 <gamrit450@gmail.com>
2023-02-19 10:54:16 +0545

    loop-statement

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        //for loop
+//        for(int money = 0; money<100; money++){
+//            System.out.println("Money" +money);
+//        }
+
+        //while
+//        int money=0;
+//        while( money!=100)
+//        {
+//            System.out.println("money"+money);
+//            money++;
+//        }
+
+        //do-while
+//        int money = 1;
+//        do {
+//            System.out.println("money" +money);
+//            money++;
+//        } while(money!=100);
+
+//        for loop
+        int[] age = {40, 50, 60};
+
+        for(int element:age) {
+            System.out.println("Element >>>" +  element);
+        }
+    }
+}
